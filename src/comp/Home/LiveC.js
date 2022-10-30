70b1f91267be0b43b546850e4df6bf2db70c9e36
Raj kumar <rajkumar@inclusivegrowthchain.com>
2022-10-30 15:37:44 +0530

    live data and layout update

--- src/comp/Home/LiveC.js
+++ src/comp/Home/LiveC.js
@@ -2,7 +2,7 @@ import live from "../../constants/live";
 
 function LiveC() {
   return (
-    <section className="live dfc h-full border-r border-[rgba(255,255,255,.3)] overflow-y-hidden">
+    <section className="dfc h-full border-r border-[rgba(255,255,255,.3)] overflow-y-hidden">
       <div className="df py-2 px-6 border-b border-[rgba(255,255,255,.3)]">
         <h1 className="flex-auto text-xl font-medium text-center">Live Market</h1>
 
@@ -16,14 +16,15 @@ function LiveC() {
         <table className="w-full table-fixed">
           <thead>
             <tr className="sticky top-0 text-sm bg-slate-900 shadow-[0_1px_3px_0_rgba(255,255,255,.1)] z-1">
-              <td className="px-4 py-2">Bond Details</td>
-              <td className="px-4 py-2">Coupon Rate</td>
-              <td className="px-4 py-2">Face Value</td>
-              <td className="px-4 py-2">LTP</td>
-              <td className="px-4 py-2">Credit Rating</td>
-              <td className="px-4 py-2">Maturity Date</td>
-              <td className="px-4 py-2 text-center">Bid Price</td>
-              <td className="px-4 py-2 text-center">Ask Price</td>
+              <td className="w-36 px-4 py-2">Security Code</td>
+              <td className="w-52 px-4 py-2">Issuer Name</td>
+              <td className="w-32 px-4 py-2">Coupon Rate</td>
+              <td className="w-28 px-4 py-2">Face Value</td>
+              <td className="w-24 px-4 py-2">LTP</td>
+              <td className="w-80 px-4 py-2">Credit Rating</td>
+              <td className="w-32 px-4 py-2 text-center">Maturity Date</td>
+              <td className="w-28 px-4 py-2 text-center">Bid Price</td>
+              <td className="w-28 px-4 py-2 text-center">Ask Price</td>
             </tr>
           </thead>
 
@@ -31,20 +32,13 @@ function LiveC() {
             {
               live.map((li, i) => (
                 <tr key={i} className="hover:bg-[rgba(255,255,255,.1)] cursor-pointer group">
-                  <td className="px-4 py-2 text-sm opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100">
-                    <div>
-                      <p className="mb-0.5">{li.details.name}</p>
-                      <p className="w-fit text-xs border border-emerald-700">
-                        <span className="px-1 py-0.5 font-light bg-emerald-400 text-white">CPN</span>
-                        <span className="px-2 py-0.5">{li.details.cpn}</span>
-                      </p>
-                    </div>
-                  </td>
+                  <td className="px-4 py-2 text-sm opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100"> {li.securityCode} </td>
+                  <td className="px-4 py-2 text-sm font-medium opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100"> {li.issuerName} </td>
                   <td className="px-4 py-2 text-sm opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100"> {li.couponRate} </td>
-                  <td className="px-4 py-2 text-sm opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100"> {li.facevalue} </td>
+                  <td className="px-4 py-2 text-sm opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100"> {li.faceValue} </td>
                   <td className="px-4 py-2 text-sm opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100"> {li.ltp} </td>
-                  <td className="px-4 py-2 text-sm opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100"> {li.creditRating} </td>
-                  <td className="px-4 py-2 text-sm opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100"> {li.maturityDate} </td>
+                  <td className="px-4 py-2 text-xs opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100"> {li.creditRating} </td>
+                  <td className="px-4 py-2 text-sm opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100 text-center"> {li.maturityDate} </td>
                   <td className="px-4 py-2 text-sm opacity-80 border-b border-[rgba(255,255,255,.3)] group-hover:opacity-100 text-center">
                     <button className="w-20 px-3 py-1.5 rounded border border-emerald-600">
                       {li.bidPrice}
--- src/comp/Home/index.js
+++ src/comp/Home/index.js
@@ -1,13 +1,11 @@
-import Sidebar from './Sidebar';
 import LiveC from './LiveC';
 import Nav from './Nav';
 
 function Home() {
   return (
-    <div className="home h-screen bg-slate-900 text-white overflow-hidden">
+    <div className="h-screen bg-slate-900 text-white overflow-hidden">
       <Nav />
       <LiveC />
-      <Sidebar />
     </div>
   )
 }
